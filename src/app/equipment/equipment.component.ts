3a9a727391b412530a4716f6e319afd9c6c5865e
John Wasielewicz <wazjohn@yahoo.com>
2020-08-05 20:02:31 -0500

    Worked on one of the bonus missions.

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -8,15 +8,15 @@ import { isNgTemplate } from '@angular/compiler';
 })
 export class EquipmentComponent implements OnInit {
    equipmentItems: object[] = [
-       {name: 'Duct Tape', mass: 0.5},
-       {name: 'Space Camera', mass: 20},
-       {name: 'Food', mass: 150},
-       {name: 'Oxygen Tanks', mass: 400},
-       {name: 'AE-35 Unit', mass: 5},
-       {name: 'ISS Supplies', mass: 800},
-       {name: 'Water', mass: 250},
-       {name: 'Satellite', mass: 1200},
-       {name: 'R2 Unit', mass: 32}
+       {name: 'Duct Tape', mass: 0.5, count: 0},
+       {name: 'Space Camera', mass: 20, count: 0},
+       {name: 'Food', mass: 150, count: 0},
+       {name: 'Oxygen Tanks', mass: 400, count: 0},
+       {name: 'AE-35 Unit', mass: 5, count: 0},
+       {name: 'ISS Supplies', mass: 800, count: 0},
+       {name: 'Water', mass: 250, count: 0},
+       {name: 'Satellite', mass: 1200, count: 0},
+       {name: 'R2 Unit', mass: 32, count: 0}
    ];
    cargoHold: object[] = [];
    cargoMass: number = 0;
@@ -37,8 +37,18 @@ export class EquipmentComponent implements OnInit {
      return this.maximumAllowedMass - 200 < this.cargoMass;
    }
 
+   clickCount(item: object): void {
+     item['count']++;
+   }
+
+   resetClickCount(): void {
+     for (let i = 0; i < this.equipmentItems.length; i++) {
+       this.equipmentItems[i]['count'] = 0;
+     }
+   }
+
    disableButton(item) {
-     return this.cargoHold.length === this.maxItems || this.cargoMass + item.mass > this.maximumAllowedMass;
+     return this.cargoHold.length === this.maxItems || this.cargoMass + item.mass > this.maximumAllowedMass  || item.count === 2;
    }
 
    activeButtons(item) {
